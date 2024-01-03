b08645b15bc846d037f13cf5a72e929589381f2f
Radhika-222 <wandareradhika@gmail.com>
2024-01-03 22:18:55 +0530

    Ph3CourseEndProject1

---
+++ CourseEndProject1_EMS/CourseEndProject1_EMS/Data/DeptMasterDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CourseEndProject1_EMS.Models;
+
+namespace CourseEndProject1_EMS.Data
+{
+    public class DeptMasterDbContext : DbContext
+    {
+        public DeptMasterDbContext (DbContextOptions<DeptMasterDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CourseEndProject1_EMS.Models.DeptMaster> DeptMaster { get; set; } = default!;
+    }
+}
---
+++ CourseEndProject1_EMS/CourseEndProject1_EMS/Data/EmpProfileDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CourseEndProject1_EMS.Models;
+
+namespace CourseEndProject1_EMS.Data
+{
+    public class EmpProfileDbContext : DbContext
+    {
+        public EmpProfileDbContext (DbContextOptions<EmpProfileDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CourseEndProject1_EMS.Models.EmpProfile> EmpProfile { get; set; } = default!;
+    }
+}
